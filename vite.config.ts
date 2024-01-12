33f1b515ec922331f8656e90ce7a6166f4451e4e
hzrd149 <github@hzrd149.com>
2024-01-12 13:21:15 +0000

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "example",
+  base: "/nostr-idb",
   plugins: [react()],
   build: {
     target: "es2022",
